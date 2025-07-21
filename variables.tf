e7bb9b5eb92a35785543529913c689ff3207ab8e
Juhika14 <61896419+Juhika14@users.noreply.github.com>
2025-07-21 15:25:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-                                                                                                                                                               
 variable "aws_region" {
   description = "AWS region"
   type        = string
@@ -31,4 +30,22 @@ variable "public_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-                                                                                        
+}
+
+variable "private_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+variable "blocked_ip" {
+  description = "IP to block in stateful firewall"
+  type        = string
+  default     = "198.51.100.1/32"
+}
+
+variable "environment" {
+  description = "Resource environment tag"
+  type        = string
+  default     = "dev"
+}
